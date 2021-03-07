cec9ec21d8445609292c18e33da9cffa5d580da2
Yadhav Jayaraman <57544838+decyjphr@users.noreply.github.com>
2021-03-07 09:03:38 -0500

    add new build

--- dist/index.js
+++ dist/index.js
@@ -2,99 +2,6 @@ require('./sourcemap-register.js');module.exports =
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 983:
-/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
-
-"use strict";
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.Semver = void 0;
-const constants_1 = __webpack_require__(105);
-const core = __importStar(__webpack_require__(186));
-class Semver {
-    constructor(currentVersion, isFirstRelease, bump, prelabel) {
-        this.prelabel = 'alpha';
-        this.currentVersion = currentVersion;
-        this.bump = bump;
-        if (prelabel) {
-            this.prelabel = prelabel;
-        }
-        this.isFirstRelease = isFirstRelease;
-    }
-    getNextVersion() {
-        if (!this.isFirstRelease) {
-            const regexstr = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm;
-            const versionparts = this.currentVersion.split(regexstr).filter(s => {
-                return s !== null && s !== '';
-            });
-            core.debug(`Version Parts are ${versionparts}`);
-            if (versionparts != null) {
-                const parts = versionparts.slice(0, 3).map(s => parseInt(s));
-                let prebuild = versionparts[3]
-                    ? versionparts[3].split('.')
-                    : [this.prelabel, '0'];
-                core.debug(`Parts are ${parts}`);
-                switch (this.bump) {
-                    case constants_1.Bumps.major:
-                        ++parts[0];
-                        parts[1] = 0;
-                        parts[2] = 0;
-                        prebuild = [];
-                        break;
-                    case constants_1.Bumps.minor:
-                        ++parts[1];
-                        parts[2] = 0;
-                        prebuild = [];
-                        break;
-                    case constants_1.Bumps.patch:
-                        ++parts[2];
-                        prebuild = [];
-                        break;
-                    case constants_1.Bumps.pre:
-                        core.debug(`Prebuild is ${prebuild}`);
-                        if (prebuild.length === 2 && prebuild[0] === this.prelabel) {
-                            prebuild[1] = (parseInt(prebuild[1]) + 1).toString();
-                        }
-                        else {
-                            prebuild[0] = this.prelabel;
-                            prebuild[1] = '1';
-                        }
-                        break;
-                    default:
-                        throw new Error(`Invalid Bump ${this.bump}`);
-                }
-                return this.bump === constants_1.Bumps.pre
-                    ? `${parts.join('.')}-${prebuild.join('.')}`
-                    : `${parts.join('.')}`;
-            }
-        }
-        return this.currentVersion;
-    }
-}
-exports.Semver = Semver;
-
-
-/***/ }),
-
 /***/ 105:
 /***/ ((__unused_webpack_module, exports) => {
 
@@ -225,7 +132,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__webpack_require__(186));
 const github = __importStar(__webpack_require__(438));
 const inputHelper = __importStar(__webpack_require__(480));
-const Semver_1 = __webpack_require__(983);
+const semver_1 = __webpack_require__(107);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -259,15 +166,15 @@ function run() {
                     };
                 }
             }
-            const semver = new Semver_1.Semver(release.data.tag_name, isFirstRelease, semverInputs.bump, semverInputs.prelabel);
+            const semver = new semver_1.Semver(release.data.tag_name, isFirstRelease, semverInputs.bump, semverInputs.prelabel);
             core.debug(`Semver is ${semver}`);
             const newTag = semver.getNextVersion();
             core.debug(`new tag = ${newTag}`);
-            // release = await octokit.repos.createRelease({
-            //   repo: github.context.repo.repo,
-            //   owner: github.context.repo.owner,
-            //   tag_name: newTag
-            // })
+            release = yield octokit.repos.createRelease({
+                repo: github.context.repo.repo,
+                owner: github.context.repo.owner,
+                tag_name: newTag
+            });
             core.debug(release.data.tag_name);
             core.setOutput('release', release.data.tag_name);
         }
@@ -277,6 +184,99 @@ function run() {
     });
 }
 run();
+
+
+/***/ }),
+
+/***/ 107:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.Semver = void 0;
+const constants_1 = __webpack_require__(105);
+const core = __importStar(__webpack_require__(186));
+class Semver {
+    constructor(currentVersion, isFirstRelease, bump, prelabel) {
+        this.prelabel = 'alpha';
+        this.currentVersion = currentVersion;
+        this.bump = bump;
+        if (prelabel) {
+            this.prelabel = prelabel;
+        }
+        this.isFirstRelease = isFirstRelease;
+    }
+    getNextVersion() {
+        if (!this.isFirstRelease) {
+            const regexstr = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm;
+            const versionparts = this.currentVersion.split(regexstr).filter(s => {
+                return s !== null && s !== '';
+            });
+            core.debug(`Version Parts are ${versionparts}`);
+            if (versionparts != null) {
+                const parts = versionparts.slice(0, 3).map(s => parseInt(s));
+                let prebuild = versionparts[3]
+                    ? versionparts[3].split('.')
+                    : [this.prelabel, '0'];
+                core.debug(`Parts are ${parts}`);
+                switch (this.bump) {
+                    case constants_1.Bumps.major:
+                        ++parts[0];
+                        parts[1] = 0;
+                        parts[2] = 0;
+                        prebuild = [];
+                        break;
+                    case constants_1.Bumps.minor:
+                        ++parts[1];
+                        parts[2] = 0;
+                        prebuild = [];
+                        break;
+                    case constants_1.Bumps.patch:
+                        ++parts[2];
+                        prebuild = [];
+                        break;
+                    case constants_1.Bumps.pre:
+                        core.debug(`Prebuild is ${prebuild}`);
+                        if (prebuild.length === 2 && prebuild[0] === this.prelabel) {
+                            prebuild[1] = (parseInt(prebuild[1]) + 1).toString();
+                        }
+                        else {
+                            prebuild[0] = this.prelabel;
+                            prebuild[1] = '1';
+                        }
+                        break;
+                    default:
+                        throw new Error(`Invalid Bump ${this.bump}`);
+                }
+                return this.bump === constants_1.Bumps.pre
+                    ? `${parts.join('.')}-${prebuild.join('.')}`
+                    : `${parts.join('.')}`;
+            }
+        }
+        return this.currentVersion;
+    }
+}
+exports.Semver = Semver;
 
 
 /***/ }),
